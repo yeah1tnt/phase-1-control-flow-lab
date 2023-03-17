7b75cf7110d9dd06064cfdd307a384f169d55ab7
Tong Tang <pichip22@hotmail.com>
2023-03-17 15:14:39 -0400

    added 3 functions

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if(num < 400) {
+    return `This one is on me!`;
+  }else if(num > 400 && num < 2000){
+    return `That will be twenty bucks.`;
+  }else if(num >2000 && num < 2500){
+    return `I will gladly take your thirty bucks.`;
+  }else{
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(name){
+  let response;
+  name === `NYC` ? response = `Ok, sounds good.` : response = `No go.`;
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case `generous`:
+      return `Thank you so much.`;
+    case `not as generous`:
+      return `Thank you.`;
+    case `thanks for everything`:
+      return `Bye.`;
+  }
 }
